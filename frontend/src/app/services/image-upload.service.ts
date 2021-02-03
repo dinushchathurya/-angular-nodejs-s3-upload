9e408cbade89075df6f2a234e6b0355b03228801
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-03 13:09:42 +0530

    :sparkles: Create image upload function

--- frontend/src/app/services/image-upload.service.ts
+++ frontend/src/app/services/image-upload.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 export class ImageUploadService {
 
   constructor(private http: HttpClient) { }
-  
+
   imageUpload(imageForm: FormData) {
     console.log('image uploading');
     return this.http.post('http://localhost:3000/api/v1/upload', imageForm);
