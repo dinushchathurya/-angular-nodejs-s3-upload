f6bc344a3d0f615da81842336f7ccdf5d47da0b6
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-03 13:11:25 +0530

    :sparkles: Create methods for pick image and upload image

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ImageUploadService } from './services/image-upload.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  imageObj: File;
+  imageUrl: string;
+
+  constructor(private imageUploadService: ImageUploadService) { }
+
+  onImagePicked(event: Event): void {
+    const FILE = (event.target as HTMLInputElement).files[0];
+    this.imageObj = FILE;
+  }
+
+  onImageUpload() {
+    const imageForm = new FormData();
+    imageForm.append('image', this.imageObj);
+    this.imageUploadService.imageUpload(imageForm).subscribe(res => {
+      this.imageUrl = res['image'];
+    });
+  }
 }
